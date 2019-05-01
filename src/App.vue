22d387929bd7a7ea6d70a8e2000e9a19edf8107c
Prodigy - <peeddurple@gmail.com>
2019-04-30 20:34:47 -0400

    spinner while promise runnning

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,63 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
-    ul
+    img.vue-logo(src='./assets/logo.png')
+
+    select(v-model="selectedCountry")
+      option(v-for="(country, index) in countries" :key="index" v-bind:value="country.value")| {{country.name}}
+    
+    spinner(v-show="showSpinner")
+    
+    ul(v-show="!showSpinner")
       artist(v-for="artist in artists" v-bind:artist="artist")
 </template>
 
 <script>
 import getArtists from './api'
 import Artist from './components/Artist.vue'
+import Spinner from './components/Spinner.vue'
 
 export default {
   name: 'app',
   data () {
     return {
-      artists: []
+      artists: [],
+      countries: [
+        {name: "Argentina", value: "argentina"},
+        {name: "Venezuela", value: "venezuela"},
+        {name: "España", value: "spain"},
+        {name: "Colombia", value: "colombia"},
+      ],
+      selectedCountry: "argentina",
+      showSpinner: false
     }
   },
   components:{
-    Artist
+    Artist,
+    Spinner
+  },
+  methods: {
+    refreshArtists(){
+      const self = this
+      self.showSpinner = true;    
+      getArtists(this.selectedCountry)
+        .then(function(artists){
+          self.artists = artists; 
+          self.showSpinner = false;
+        });  
+    }
   },
   mounted: function(){
-    const self = this
-    getArtists()
-      .then(function(artists){
-        self.artists = artists; 
-      });
+    this.refreshArtists()
+  },
+  watch:{
+    selectedCountry(){
+      this.refreshArtists()
+    }
   }
 }
 </script>
 
-<style lang="stylus">
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -55,5 +83,15 @@ li {
 
 a {
   color: #42b983;
+}
+
+select{
+  display: block;
+  margin: auto;
+}
+
+.vue-logo{
+  display: block;
+  margin: auto;
 }
 </style>
--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -1,18 +1,9 @@
-<!--template lang="pug">
-    li
-        h2: a(:href="artist.url" target="_blank")wer
-        img(:src="artist.image[2]['#text']")
-</template-->
-<!--template>
-    <li>
-    {{artist.name}}
-    </li>
-</template-->
 <template lang="pug">
     li
-        h2: a(:href="artist.url" target="_blank")
-            |   {{artist.name}}
-        img(:src="artist.image[2]['#text']")
+        a(:href="artist.url" target="_blank")
+            h2
+                |   {{artist.name}}
+            img(:src="artist.image[2]['#text']")
 </template>
 
 <script>
